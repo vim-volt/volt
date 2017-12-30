7c78166232c3a7de85cf054992e1a83de3f4687b
tyru <tyru.exe@gmail.com>
2017-12-30 22:39:41 +0900

    Call subtests serially to control execution order
    `go test -v -race -parallel 3` complains as follows:
    
        WARNING: DATA RACE
        Read at 0x000000e17470 by goroutine 49:
          github.com/vim-volt/volt/cmd.setupOutput()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:70 +0x7d
          github.com/vim-volt/volt/cmd.testVoltSelfUpgradeCheckFromCurrentVer()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:48 +0x50
          testing.tRunner()
              /home/travis/.gimme/versions/go1.9.linux.amd64/src/testing/testing.go:746 +0x16c
        Previous write at 0x000000e17470 by goroutine 75:
          github.com/vim-volt/volt/cmd.setupOutput.func2()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:86 +0x66
        Goroutine 49 (running) created at:
          testing.(*T).Run()
              /home/travis/.gimme/versions/go1.9.linux.amd64/src/testing/testing.go:789 +0x568
          github.com/vim-volt/volt/cmd.TestVoltSelfUpgrade()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:16 +0x8c
          testing.tRunner()
              /home/travis/.gimme/versions/go1.9.linux.amd64/src/testing/testing.go:746 +0x16c
        Goroutine 75 (finished) created at:
          github.com/vim-volt/volt/cmd.setupOutput()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:83 +0x1b2
          github.com/vim-volt/volt/cmd.testVoltSelfUpgradeCheckFromOldVer()
              /home/travis/gopath/src/github.com/vim-volt/volt/cmd/self_upgrade_test.go:27 +0xe9
          testing.tRunner()
              /home/travis/.gimme/versions/go1.9.linux.amd64/src/testing/testing.go:746 +0x16c

--- cmd/self_upgrade_test.go
+++ cmd/self_upgrade_test.go
@@ -8,53 +8,53 @@ import (
 	"testing"
 )
 
+func TestVoltSelfUpgrade(t *testing.T) {
+	// Calling subtests serially to control execution order.
+	// These tests rewrite os.Stdout/os.Stderr so running them parallely may cause
+	// unexpected behavior.
+	t.Run("testVoltSelfUpgradeCheckFromOldVer", testVoltSelfUpgradeCheckFromOldVer)
+	t.Run("testVoltSelfUpgradeCheckFromCurrentVer", testVoltSelfUpgradeCheckFromCurrentVer)
+}
+
 // 'volt self-upgrade -check' from old version should show the latest release
-func TestVoltSelfUpgradeCheckFromOldVer(t *testing.T) {
+func testVoltSelfUpgradeCheckFromOldVer(t *testing.T) {
 	// =============== setup =============== //
 
 	oldVersion := voltVersion
 	voltVersion = "v0.0.1"
 	defer func() { voltVersion = oldVersion }()
 
-	outCh, done := setupOutput(t)
-	defer close(done)
-
 	// =============== run =============== //
 
-	code := Run("self-upgrade", []string{"-check"})
+	var code int
+	out := captureOutput(t, func() {
+		code = Run("self-upgrade", []string{"-check"})
+	})
+
 	if code != 0 {
 		t.Fatal("Expected exitcode=0, but got: " + strconv.Itoa(code))
 	}
-
-	done <- true
-	out := <-outCh
 	if !strings.Contains(out, "---") {
 		t.Fatal("Expected release notes, but got: " + out)
 	}
 }
 
 // 'volt self-upgrade -check' from current version should show the latest release
-func TestVoltSelfUpgradeCheckFromCurrentVer(t *testing.T) {
-	// =============== setup =============== //
+func testVoltSelfUpgradeCheckFromCurrentVer(t *testing.T) {
+	var code int
+	out := captureOutput(t, func() {
+		code = Run("self-upgrade", []string{"-check"})
+	})
 
-	outCh, done := setupOutput(t)
-	defer close(done)
-
-	// =============== run =============== //
-
-	code := Run("self-upgrade", []string{"-check"})
 	if code != 0 {
 		t.Fatal("Expected exitcode=0, but got: " + strconv.Itoa(code))
 	}
-
-	done <- true
-	out := <-outCh
 	if out != "[INFO] No updates were found.\n" {
 		t.Fatal("Expected no updates found, but got: " + out)
 	}
 }
 
-func setupOutput(t *testing.T) (chan string, chan bool) {
+func captureOutput(t *testing.T, f func()) string {
 	r, w, err := os.Pipe()
 	if err != nil {
 		t.Fatal("os.Pipe() failed: " + err.Error())
@@ -71,12 +71,11 @@ func setupOutput(t *testing.T) (chan string, chan bool) {
 		}
 		outCh <- string(b)
 	}()
-	done := make(chan bool)
-	go func() {
-		<-done
-		w.Close()
-		os.Stdout = oldStdout
-		os.Stderr = oldStderr
-	}()
-	return outCh, done
+
+	f()
+
+	w.Close()
+	os.Stdout = oldStdout
+	os.Stderr = oldStderr
+	return <-outCh
 }
