4d77f86d00287dd9589ab5f9bb1e9e5712fdabf6
tyru <tyru.exe@gmail.com>
2018-04-22 20:39:59 +0900

    fix: fix bug that operator name is not deparsed

--- dsl/deparse.go
+++ dsl/deparse.go
@@ -2,8 +2,8 @@ package dsl
 
 import (
 	"encoding/json"
-	"fmt"
 
+	"github.com/pkg/errors"
 	"github.com/vim-volt/volt/dsl/ops"
 	"github.com/vim-volt/volt/dsl/types"
 )
@@ -30,30 +30,31 @@ func deparse(value types.Value) (interface{}, error) {
 	case types.Number:
 		return val.Value(), nil
 	case types.Object:
-		m := make(map[string]interface{}, len(val.Value()))
+		result := make(map[string]interface{}, len(val.Value()))
 		for k, o := range val.Value() {
 			v, err := deparse(o)
 			if err != nil {
 				return nil, err
 			}
-			m[k] = v
+			result[k] = v
 		}
-		return m, nil
+		return result, nil
 	case types.Expr:
-		a := make([]interface{}, 0, len(val.Args())+1)
+		args := val.Args()
+		result := make([]interface{}, 0, len(args)+1)
 		// Do not include "@" in array literal
 		if val.Op().String() != ops.ArrayOp.String() {
-			a = append(a, types.NewString(val.Op().String()))
+			result = append(result, val.Op().String())
 		}
-		for i := range a {
-			v, err := deparse(val.Args()[i])
+		for i := range args {
+			v, err := deparse(args[i])
 			if err != nil {
 				return nil, err
 			}
-			a = append(a, v)
+			result = append(result, v)
 		}
-		return a, nil
+		return result, nil
 	default:
-		return nil, fmt.Errorf("unknown value was given '%+v'", val)
+		return nil, errors.Errorf("unknown value was given '%+v'", val)
 	}
 }
