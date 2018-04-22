08bc03d8c09c3675178357f85ec3d33e9442f78e
tyru <tyru.exe@gmail.com>
2018-04-22 18:10:01 +0900

    refactor: fix comments

--- dsl/ops/macro.go
+++ dsl/ops/macro.go
@@ -6,7 +6,6 @@ import (
 
 type macroBase string
 
-// String returns "$array"
 func (m *macroBase) String() string {
 	return string(*m)
 }
@@ -15,7 +14,7 @@ func (*macroBase) IsMacro() bool {
 	return true
 }
 
-// Invert the result of op.Execute() which expands an expression
+// macroInvertExpr inverts the result of op.Execute() which expands an expression
 func (*macroBase) macroInvertExpr(val types.Value, _ func(), err error) (types.Value, error) {
 	if err != nil {
 		return nil, err
