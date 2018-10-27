348f6ea5553d7fa008bbb9b9240e69634e2616fe
tyru <tyru.exe@gmail.com>
2018-10-28 00:33:43 +0900

    fix: fix compile error of test code

--- gateway/self_upgrade_test.go
+++ gateway/self_upgrade_test.go
@@ -5,6 +5,10 @@ import (
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/vim-volt/volt/config"
+	"github.com/vim-volt/volt/lockjson"
 )
 
 func TestVoltSelfUpgrade(t *testing.T) {
@@ -30,8 +34,7 @@ func testVoltSelfUpgradeCheckFromOldVer(t *testing.T) {
 
 	var err *Error
 	out := captureOutput(t, func() {
-		args := []string{"volt", "self-upgrade", "-check"}
-		err = Run(args, DefaultRunner)
+		err = runVolt(t, "self-upgrade", "-check")
 	})
 
 	if err != nil {
@@ -46,8 +49,7 @@ func testVoltSelfUpgradeCheckFromOldVer(t *testing.T) {
 func testVoltSelfUpgradeCheckFromCurrentVer(t *testing.T) {
 	var err *Error
 	out := captureOutput(t, func() {
-		args := []string{"volt", "self-upgrade", "-check"}
-		err = Run(args, DefaultRunner)
+		err = runVolt(t, "self-upgrade", "-check")
 	})
 
 	if err != nil {
@@ -82,4 +84,24 @@ func captureOutput(t *testing.T, f func()) string {
 	os.Stdout = oldStdout
 	os.Stderr = oldStderr
 	return <-outCh
+}
+
+func runVolt(t *testing.T, cmd string, args ...string) *Error {
+	c := LookUpCmd(cmd)
+	if c == nil {
+		t.Fatal("unknown command '" + cmd + "'")
+	}
+	lockJSON, err := lockjson.Read()
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "failed to read lock.json").Error())
+	}
+	cfg, err := config.Read()
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "failed to read config.toml").Error())
+	}
+	return c.Run(&CmdContext{
+		Args:     args,
+		LockJSON: lockJSON,
+		Config:   cfg,
+	})
 }
