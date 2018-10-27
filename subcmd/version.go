053e6dc894d6393984a2596beac6c4e345c82a09
tyru <tyru.exe@gmail.com>
2018-10-27 09:30:01 +0900

    Bump version: v0.3.5 -> v0.3.6-alpha

--- subcmd/version.go
+++ subcmd/version.go
@@ -10,7 +10,7 @@ import (
 )
 
 // This variable is not constant for testing (to change it temporarily)
-var voltVersion = "v0.3.5"
+var voltVersion = "v0.3.6-alpha"
 
 func init() {
 	cmdMap["version"] = &versionCmd{}
